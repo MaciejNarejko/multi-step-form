74048debbfdb06d304f51f3f512a970aed4ae498
Maciej Narejko <maciejnarejko@gmail.com>
2023-06-19 03:02:50 +0200

    Configure Multi-step Form for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 import Form from './pages/Form/Form'
 
 function App() {
 	return (
 		<div className='App'>
-			<Router>
-				<Form />
-			</Router>
+			<Form />
 		</div>
 	)
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import './styles/global.scss'
 import App from './App'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-const root = createRoot(document.getElementById('root') as Element);
-root.render(<App />);
+const root = createRoot(document.getElementById('root') as Element)
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+)
